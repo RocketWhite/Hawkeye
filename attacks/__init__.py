f1206787372a86cb2e740c35d615fea844b3b122
RocketWhite <51713050+RocketWhite@users.noreply.github.com>
2020-03-30 20:20:43 -0400

    update attack methods

--- attacks/__init__.py
+++ attacks/__init__.py
@@ -1,3 +1,11 @@
-from .fgsm import FGSM, iFGSM
+from .attacks.fgsm import FGSM
+from .attacks.bim import BIM
+from .attacks.stepll import StepLL
+from .attacks.rfgsm import RFGSM 
+from .attacks.cw import CW
+from .attacks.pgd import PGD
+from .attacks.apgd import APGD
+from .attacks.rpgd import RPGD
+from .attacks.deepfool import DeepFool
 
-__all__ = ('FGSM', 'iFGSM')
+__version__ = 1.0
