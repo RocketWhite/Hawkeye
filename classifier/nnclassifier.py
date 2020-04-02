d418333eb4704922a357b1993b5532cd6f5a0f22
RocketWhite <51713050+RocketWhite@users.noreply.github.com>
2020-04-01 23:40:36 -0400

    v0.9 code finish, require debug and tuning

---
+++ classifier/nnclassifier.py
@@ -0,0 +1,14 @@
+import torch
+from torch import nn
+
+
+class NNClassifier(nn.Module):
+    def __init__(self):
+        super(NNClassifier, self).__init__()
+        self.conv
+
+
+    def predict(self, X):
+
+    def fit(self, X, y):
+
